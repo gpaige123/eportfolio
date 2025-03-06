5156d344a31f24deb9b293b07225f5c3262de507
gpaige123 <123510148+gpaige123@users.noreply.github.com>
2025-03-05 21:36:47 -0700

    Create theme.js

---
+++ assets/js/theme.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("theme-toggle");
+    const currentTheme = localStorage.getItem("theme");
+
+    if (currentTheme === "dark") {
+        document.body.classList.add("dark");
+    }
+
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+        localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
+    });
+});
